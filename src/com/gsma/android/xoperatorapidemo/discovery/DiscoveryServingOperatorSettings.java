4f841c44f749d773b7013ff68f0c620ceec73ea2
GSMADeveloper <stephen.doyle@mobile-life.com>
2014-09-11 13:10:02 +0100

    Addition of auto serving operator from device MCC/MNC

--- src/com/gsma/android/xoperatorapidemo/discovery/DiscoveryServingOperatorSettings.java
+++ src/com/gsma/android/xoperatorapidemo/discovery/DiscoveryServingOperatorSettings.java
@@ -7,9 +7,10 @@ public class DiscoveryServingOperatorSettings {
 	private static final ServingOperatorSetting dialog_lk=new ServingOperatorSetting("Dialog Sri Lanka (413-02)", false, "413", "02", null);
 	private static final ServingOperatorSetting mobitel_lk=new ServingOperatorSetting("Mobitel Sri Lanka (413-01)", false, "413", "01", null);
 	private static final ServingOperatorSetting telia_se=new ServingOperatorSetting("Telia Sweden (240-01)", false, "240", "01", null);
+	private static final ServingOperatorSetting auto=new ServingOperatorSetting("Auto (device MCC/MNC)", true, null, null, "5.5.5.5");
 	private static final ServingOperatorSetting none=new ServingOperatorSetting("No auto assist", false, null, null, "5.5.5.5");
 	
-	private static final ServingOperatorSetting[] operators={etel, telia_se, dialog_lk, mobitel_lk, none};
+	private static final ServingOperatorSetting[] operators={auto, etel, telia_se, dialog_lk, mobitel_lk, none};
 	
 	private static String[] operatorNames=null;
 	
